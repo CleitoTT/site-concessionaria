02c447f1b2ca89f1af36e41c6095ad6acfb0cdba
Leonardo <leo.msouza1706@gmail.com>
2024-06-16 16:11:20 -0300

    Melhoria de class tailwind

--- src/components/AreaResultado/index.js
+++ src/components/AreaResultado/index.js
@@ -3,7 +3,7 @@ import Button from "../Button"
 
 export default function AreaResultado(){
     return(
-        <div className="w-[350px] h-[350px] ml-16 mr-5 border-4 boder-solid rounded-3xl border-[#E10029] bg-transparent flex flex-col justify-center items-center">
+        <div className="size-[350px] ml-16 mr-5 border-4 boder-solid rounded-3xl border-[#E10029] bg-transparent flex flex-col justify-center items-center">
             <h1 className="font-bold text-3xl text-[#E10029] mb-3  font-sans">Nome do carro</h1>
             <p className="text-[#E10029] text-lg text-center font-medium mb-6">Modelo do carro</p>
             <img src={carro} alt="imagem do carro"/>
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,7 +1,7 @@
 export default function Logo(){
     return(
         <div className="flex">
-            <div className="w-10 h-10 bg-[#AA0000] border-4 border-solid border-[#E10029] mr-2"></div>
+            <div className="size-10 bg-[#AA0000] border-4 border-solid border-[#E10029] mr-2"></div>
             <p className="text-3xl text-[#E10029] font-medium">Revive<b>Auto</b></p>
         </div>
     )

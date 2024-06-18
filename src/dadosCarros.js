cd3c95be5291fef2005ebe9ff965b586c6981e53
Leonardo <leo.msouza1706@gmail.com>
2024-06-18 16:37:38 -0300

    Adição de arquivo de dado carros

---
+++ src/dadosCarros.js
@@ -0,0 +1,8 @@
+export const carros = [
+    {nome: "Corsa", marca: "Chevrolet", modelo: "Sedan"},
+    {nome: "Corolla", marca: "Toyota", modelo:"Sedan"},
+    {nome: "Uno", marca: "Fiat", modelo:"hatch"},
+    {nome: "208", marca: "Peugeot", modelo: "hatch"},
+    {nome: "SW4", marca: "Toyota", modelo: "SUV"},
+    {nome: "T-Cross", marca: "Volkswagem", modelo: "SUV"}
+]

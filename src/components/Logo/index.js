30c64b096ba4e5c6cb793c7cafd0ba6f33013dc3
Leonardo Machado <leo.msouza1706@gmail.com>
2024-07-08 19:14:04 -0300

    Correção de tamanho logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,9 +2,9 @@ import logo from "../../images/logo-reviveauto.png"
 
 export default function Logo(){
     return(
-        <div className="flex">
-            <img src={logo} alt="Imagem logo" />
-            <p className="text-3xl text-[#FE0000] font-medium">Revive<b>Auto</b></p>
+        <div className="flex items-center">
+            <img src={logo} alt="Imagem logo"  className="w-16 h-20 mr-[-15px]"/>
+            <p className="text-3xl text-[#FE0000] font-medium">evive<b>Auto</b></p>
         </div>
     )
 }

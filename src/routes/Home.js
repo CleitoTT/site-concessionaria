ec562cc230be1110cd329b031d1f51313c4a7494
Leonardo Machado <leo.msouza1706@gmail.com>
2024-07-15 15:23:28 -0300

    Correção altura height

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import AreaCentro from "../components/AreaCentro";
 
 export default function Home() {
   return (
-    <div className="w-full h-[873px] bg-[#E10029] flex flex-col items-center">
+    <div className="w-full h-screen bg-[#E10029] flex flex-col items-center">
       <AreaCentro />
     </div>
   );

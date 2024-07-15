619d5d478fb13c9b4e55ed767f92292217d35b19
Leonardo Machado <leo.msouza1706@gmail.com>
2024-07-15 15:50:08 -0300

    Alteração responsividade pt.3

--- src/components/OpcoesHeader/index.js
+++ src/components/OpcoesHeader/index.js
@@ -7,11 +7,11 @@ export default function OpcoesHeader(){
         <>
             <div className="list-none flex text-center text-[#E10029] font-medium text-xl cursor-pointer mb-5 lg:mb-0">
                 <Link to={`/`}>
-                        <p className="mr-5 hover:scale-110 duration-500">HOME</p>
+                        <p className="lg:mr-5 hover:scale-110 duration-500">HOME</p>
                 </Link>
                 {textos.map(item => (
                     <Link to={`/${item.toLowerCase()}`}>
-                        <p className="mr-5 hover:scale-110 duration-500">{item}</p>
+                        <p className="lg:mr-5 hover:scale-110 duration-500">{item}</p>
                     </Link>
                 ))}
             </div>
